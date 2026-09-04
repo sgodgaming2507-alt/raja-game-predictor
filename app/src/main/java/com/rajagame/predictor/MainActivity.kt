bd080a6f32e2a660df8087d247e13ec2210266c4
sgodgaming2507-alt <sgodgaming2507@gmail.com>
2026-09-05 00:59:38 +0530

    Update MainActivity.kt

--- app/src/main/java/com/rajagame/predictor/MainActivity.kt
+++ app/src/main/java/com/rajagame/predictor/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.rajagame.predictor
 
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnStart = findViewById<Button>(R.id.startBtn)
+        btnStart.setOnClickListener {
+            if (checkOverlayPermission()) {
+                startFloatingService()
+            }
+        }
+    }
+
+    private fun checkOverlayPermission(): Boolean {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
+            val intent = Intent(
+                Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                Uri.parse("package:$packageName")
+            )
+            startActivity(intent)
+            return false
+        }
+        return true
+    }
+
+    private fun startFloatingService() {
+        val intent = Intent(this, FloatingService::class.java)
+        startService(intent)
+        finish() // App open hote hi floating window chalu ho jayegi aur main activity minimize ho jayegi
     }
 }
